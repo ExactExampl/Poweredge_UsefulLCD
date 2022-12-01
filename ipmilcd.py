e5d4fcbcd5801bfa6b1457712af6524e7991b426
ExactExampl <64069095+ExactExampl@users.noreply.github.com>
2022-12-01 17:48:43 +0200

    ipmilcd: do temperature counting according to threads number
    * Socket 1366 have dual, quad and six core cpu`s
    
    * Determine which cpu setup we have by number of threads (will work correctly only for dual cpu setups)

--- ipmilcd.py
+++ ipmilcd.py
@@ -1,9 +1,11 @@
 from time import sleep
 from subprocess import Popen
 from sys import exit
+from multiprocessing import cpu_count
 import psutil, signal
 
 translator = '/usr/sbin/ipmilcd/lcd.pl'
+threads_num = cpu_count()
 stopped = False
 servname = 'change me' # Server name (must be 14 characters long)
 
@@ -22,27 +24,57 @@ signal.signal(signal.SIGINT, handle_stop)
 
 def cputemp():
     while not stopped:
-        t1 = psutil.sensors_temperatures()["coretemp"][0]
-        t2 = psutil.sensors_temperatures()["coretemp"][1]
-        t3 = psutil.sensors_temperatures()["coretemp"][2]
-        t4 = psutil.sensors_temperatures()["coretemp"][3]
-        t5 = psutil.sensors_temperatures()["coretemp"][4]
-        t6 = psutil.sensors_temperatures()["coretemp"][5]
-        t7 = psutil.sensors_temperatures()["coretemp"][6]
-        t8 = psutil.sensors_temperatures()["coretemp"][7]
-        t9 = psutil.sensors_temperatures()["coretemp"][8]
-        t10 = psutil.sensors_temperatures()["coretemp"][9]
-        t11 = psutil.sensors_temperatures()["coretemp"][10]
-        t12 = psutil.sensors_temperatures()["coretemp"][11]
+        if threads_num == 24 or threads_num == 12: # 2 six-core cpu`s case
+            t1 = psutil.sensors_temperatures()["coretemp"][0]
+            t2 = psutil.sensors_temperatures()["coretemp"][1]
+            t3 = psutil.sensors_temperatures()["coretemp"][2]
+            t4 = psutil.sensors_temperatures()["coretemp"][3]
+            t5 = psutil.sensors_temperatures()["coretemp"][4]
+            t6 = psutil.sensors_temperatures()["coretemp"][5]
+            t7 = psutil.sensors_temperatures()["coretemp"][6]
+            t8 = psutil.sensors_temperatures()["coretemp"][7]
+            t9 = psutil.sensors_temperatures()["coretemp"][8]
+            t10 = psutil.sensors_temperatures()["coretemp"][9]
+            t11 = psutil.sensors_temperatures()["coretemp"][10]
+            t12 = psutil.sensors_temperatures()["coretemp"][11]
+            
+            tlist1 = [int(t1.current), int(t2.current), int(t3.current), 
+                      int(t4.current), int(t5.current), int(t6.current)]
+            approxtemp1 = int(sum(tlist1) / len(tlist1))
+            
+            tlist2 = [int(t7.current), int(t8.current), int(t9.current), 
+                      int(t10.current), int(t11.current), int(t12.current)]
+            approxtemp2 = int(sum(tlist2) / len(tlist2))
         
-        tlist1 = [int(t1.current), int(t2.current), int(t3.current), 
-                  int(t4.current), int(t5.current), int(t6.current)]
-        approxtemp1 = int(sum(tlist1) / len(tlist1))
-        
-        tlist2 = [int(t7.current), int(t8.current), int(t9.current), 
-                  int(t10.current), int(t11.current), int(t12.current)]
-        approxtemp2 = int(sum(tlist2) / len(tlist2))
-        
+        elif threads_num == 8 or threads_num == 4: # 2 dual core cpu`s case`
+            t1 = psutil.sensors_temperatures()["coretemp"][0]
+            t2 = psutil.sensors_temperatures()["coretemp"][1]
+            t3 = psutil.sensors_temperatures()["coretemp"][2]
+            t4 = psutil.sensors_temperatures()["coretemp"][3]
+            
+            tlist1 = [int(t1.current), int(t2.current)]
+            approxtemp1 = int(sum(tlist1) / len(tlist1))
+            
+            tlist2 = [int(t3.current), int(t4.current)]
+            approxtemp2 = int(sum(tlist2) / len(tlist2))
+            
+        else: # else there is only two quad core cpu`s case left
+            t1 = psutil.sensors_temperatures()["coretemp"][0]
+            t2 = psutil.sensors_temperatures()["coretemp"][1]
+            t3 = psutil.sensors_temperatures()["coretemp"][2]
+            t4 = psutil.sensors_temperatures()["coretemp"][3]
+            t5 = psutil.sensors_temperatures()["coretemp"][4]
+            t6 = psutil.sensors_temperatures()["coretemp"][5]
+            t7 = psutil.sensors_temperatures()["coretemp"][6]
+            t8 = psutil.sensors_temperatures()["coretemp"][7]
+            
+            tlist1 = [int(t1.current), int(t2.current), int(t3.current),
+                      int(t4.current)]
+            approxtemp1 = int(sum(tlist1) / len(tlist1))
+            
+            tlist2 = [int(t5.current), int(t6.current), int(t7.current), 
+                      int(t8.current)]
+            approxtemp2 = int(sum(tlist2) / len(tlist2))
         return approxtemp1, approxtemp2
     
 cptemp1, cptemp2 = cputemp()
